238a7c41ac0d0f0e20fb979deb43d5fb7b214c7e
sumon092 <m.sumon@outlook.com>
2022-05-24 16:19:08 +0600

    parts section added

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require('cors');
 app.use(cors());
 
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xk2aa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db connected');
+        const partsCollection = client.db('Parts_man').collection('parts');
+
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('car part server is running on heroku!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`car daddy app listening on port ${port}`)
 })
