c337095e3961ed978028d121090f94eff540a33a
sumon092 <m.sumon@outlook.com>
2022-05-26 00:29:51 +0600

    order section complete

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xk2aa.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -20,6 +19,7 @@ async function run() {
         await client.connect();
         console.log('db connected');
         const partsCollection = client.db('Parts_man').collection('parts');
+        const orderCollection = client.db('Parts_man').collection('order');
 
 
         app.get('/parts', async (req, res) => {
@@ -28,6 +28,24 @@ async function run() {
             const parts = await cursor.toArray();
             res.send(parts);
         });
+        app.get('/order', async (req, res) => {
+            const query = {};
+            const cursor = orderCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const query = { name: order.partsName, price: order.price, email: order.email }
+            console.log(query);
+            const exists = await orderCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, order: exists })
+            }
+            const result = await orderCollection.insertOne(order);
+            return res.send({ success: true, result });
+        })
 
     }
     finally {
